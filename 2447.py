0bc00c33fbc84a7ab5769518988b41a140809d7a
devsungyeon <sungyeonlee1103@gmail.com>
2020-12-10 03:59:05 +0000

    today done

--- 2447.py
+++ 2447.py
@@ -1,9 +1,54 @@
-n = int(input())
-li = [ ['*','*','*'], ['*',' ','*'], ['*','*','*'] ]
+'''
+import math
 
-li = li+li
-print(li)
-for i in li:
-    for j in i:
-        print(j, end='')
+def stars(n):
+    matrix = []
+    for i in range(3*len(n)):
+        if i//len(n) == 1:
+            matrix.append(n[i % len(n)] + " " * len(n) + n[i % len(n)])
+        else:
+            matrix.append(n[i % len(n)] * 3)
+    return(list(matrix))
+
+star = ["***", "* *", "***"]
+n = int(input())
+k = 0
+k = int(math.log10(n)/math.log10(3))-1
+
+for i in range(k):
+    star = stars(star)
+for i in star:
+    print(i)
+'''
+###################
+def draw_star(n) :
+    global Map
+    
+    if n == 3 :
+        Map[0][:3] = Map[2][:3] = [1]*3
+        Map[1][:3] = [1, 0, 1]
+        return
+
+    a = n//3
+    draw_star(n//3)
+    for i in range(3) :
+        for j in range(3) :
+            if i == 1 and j == 1 :
+                continue
+            for k in range(a) :
+                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a] # 핵심 아이디어
+
+N = int(input())      
+
+# 메인 데이터 선언
+Map = [[0 for i in range(N)] for i in range(N)]
+
+draw_star(N)
+
+for i in Map :
+    for j in i :
+        if j :
+            print('*', end = '')
+        else :
+            print(' ', end = '')
     print()
