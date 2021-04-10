fd26020e9e8dbc10bbfc10589fcd13df0c0b4e41
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-10 15:17:19 +0900

    10171 done

---
+++ j10171.java
@@ -0,0 +1,5 @@
+public class j10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+	}
+}
--- name.sh
+++ name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read  fn
-echo -e "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n\t\n\treturn 0;\n}" > c$fn.cpp
-echo -e "#include <bits/stdc++.h>\n\nint main() {\n\t\n\treturn 0;\n}" > cpp$fn.cpp
+echo -e "#include <stdio.h>\n\nint main() {\n\t\n\treturn 0;\n}" > c$fn.c
+echo -e "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n\t\n\treturn 0;\n}" > cpp$fn.cpp
 echo "" > p$fn.py
 echo -e "public class j$fn {\n\tpublic static void main(String[] args) {\n\t\t\n\t}\n}" > j$fn.java
