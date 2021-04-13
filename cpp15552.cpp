c7b9bd538a836b6c06cbd551ecf5bae4e8384f93
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-13 22:38:12 +0900

    15552 done

---
+++ cpp15552.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t ;
+	cin >> t;
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	for(int i = 0 ; i < t ; i++ ) {
+		int a, b;
+		cin >> a >> b;
+		cout << a+b << "\n";
+	}
+	return 0;
+}
