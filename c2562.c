542668fc7c44e9dd5b46906edeafaa1497e49ae7
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-17 11:58:16 +0900

    ing

--- c2562.c
+++ c2562.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+
 int main() {
 	int max = 0;
 	int tmp, idx;
-	scanf("%d", &tmp);
-	idx = 1;
-	for(int i = 2 ; i <= 9 ;i++ ) {
+	idx = 0;
+	for(int i = 1 ; i <= 9 ;i++ ) {
 		scanf("%d", &tmp);
 		if(tmp > max) {
 			max = tmp;
 			idx = i;
 		}
 	}
-	printf("%d\n%d", max, idx);
+	printf("%d\n", max);
+	printf("%d", idx);
 	return 0;
 }
--- cpp2562.cpp
+++ cpp2562.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-	
+	int max, tmp;
+	max = 0;
+	tmp = 0;
+	for(int i = 1 ; i<=9; i++ ) {
+		if(tmp > max) {
+			
+		}
+	}
 	return 0;
 }
