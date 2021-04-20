60db82075e4e32d5b08d2b0d2b9118d66ef1e899
Sungyeon Lee <sungyeonlee1103@gmail.com>
2021-04-20 06:28:45 +0000

    4344 ing

--- c4344.c
+++ c4344.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
-	
+	int c;
+    scanf("%d", &c);
+    
+    int arr[1001];
+    
+    for(int i = 0 ; i < c ; i++ ) {
+        int n;
+        scanf("%d", &n);
+        int sum=0;
+        double avg;
+        for(int j = 0 ; j < n ; j++ ) {
+            int tmp ;
+            scanf("%d", &tmp);
+            arr[j] = tmp;
+            sum += tmp;
+        }
+        avg = sum / n;
+        
+        double cnt=0;
+        for(int j = 0 ; j < n ; j++ ) {
+            if(arr[j] > avg)
+                cnt++;
+        }
+        printf("%.3lf%%\n", (cnt*100)/(double)n);
+    }
+    
 	return 0;
 }
--- cpp4344.cpp
+++ cpp4344.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main() {
-	
+	int c;
+    cin >> c;
+    for(int i = 0 ; i < c ; i++ ) {
+        int n;
+        cin >> n;
+        int arr[1001];
+        int sum = 0;
+        for(int j = 0 ; j < n ; j++) {
+            int tmp;
+            cin >> tmp;
+            arr[j] = tmp;
+            sum += tmp;
+        }
+        double cnt = 0 ;
+        double avg = (double)sum/(double)n;
+        for(int j = 0 ; j < n ; j++) {
+            if(arr[j] > avg) {
+                cnt++;
+            }    
+        }
+        double per = cnt/(double)n*100;
+        cout << fixed;
+        cout.precision(3);
+        cout << per << "%" << endl;
+    }
 	return 0;
 }
