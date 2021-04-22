1bc86b82821ffb2038267b3b111f774fa126dbdf
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-22 12:26:40 +0000

    ing

--- c15596.c
+++ c15596.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main() {
+long long sum(int *a, int n) {
+	long long ret=0 ; 
 	
+	for(int i = 0 ; i < n ; i++ ) {
+		ret += *a;
+		a=a+1;
+	}
+
+	return ret;
+}
+
+int main() {
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+	int n = 10;
+
+	int answer = 0;
+
+	answer = sum(arr, n);
+
+	printf("%d\n", answer);
+
 	return 0;
 }
--- cpp15596.cpp
+++ cpp15596.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
-int main() {
+long long sum(std::vector<int> &a) {
+	long long ret = 0;
 	
+	for(int i = 0 ; i < a.size() ; i++ ) {
+		ret += a[i];
+	}
+
+	return ret;
+}
+
+int main() {
+	vector<int> arr;
+	for(int i = 1 ; i <= 10; i++ ) {
+		arr.push_back(i);
+	}
+
+	int answer = 0;
+	answer = sum(arr);
+	
+	cout << answer << endl;
+
+
 	return 0;
 }
