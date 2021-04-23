68865ecbfec91167252119982d26b14bf1b9f6c7
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-23 18:10:36 +0900

    studying

---
+++ cbasic/opentc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define TEXT "asdf"
+
+int main() {
+    int a = 30;
+    float b = 3.14;
+    char c = 'A';
+    char d[6] = {'K', 'O', 'R', 'E', 'A', '\0'};
+    printf("%d %f %c %s \n", a, b, c, d);
+
+    printf(TEXT);
+}
