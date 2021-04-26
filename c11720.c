789bd860bda038b1c8a7c9218b3aad3c1df50421
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-26 22:17:44 +0900

    11720 done

--- c11720.c
+++ c11720.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int main() {
-	
+	int n;
+	scanf("%d", &n);
+	char arr[MAX];
+
+	scanf("%s", arr);
+
+	int sum = 0 ;
+	for(int i = 0 ; i < n ; i++ ) {
+		sum += (arr[i]-'0');
+	}
+	printf("%d", sum);
+
 	return 0;
 }
--- cpp11720.cpp
+++ cpp11720.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 
+#define MAX 100
+
 using namespace std;
 
 int main() {
-	
+	int n ; 
+	cin >> n;
+	string arr;
+	cin >> arr;
+
+	int sum = 0 ;
+	for (int i = 0 ; i < n ; i++ ) {
+		sum += (arr[i]-'0');
+	}
+	cout << sum << endl;
 	return 0;
 }
