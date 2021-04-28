e824182c23fdd68184670f86872107ca20157c9a
Sungyeon Lee <sungyeonlee1103@gmail.com>
2021-04-28 07:46:12 +0000

    1157 created

---
+++ j1157.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class j1157 {
+	public static void main(String[] args) {
+		
+	}
+}
--- p1157.py
+++ p1157.py
@@ -1,9 +1,1 @@
-s = "baaa"
-#s = input()
-s = s.lower()
-if len(s) > 1:
-    li = [[chr(x), s.count(chr(x))] for x in range(97,123)]
-    li.sort(key=lambda x:x[1], reverse=True)
-    print("?" if (li[0][1] == li[1][1]) else li[0][0].upper())
-else:
-    print(s.upper())
+
