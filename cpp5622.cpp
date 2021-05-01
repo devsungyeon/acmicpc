c9db054aac6d016ba00eee45db21b6620cf4b85d
devsyl <sungyeonlee1103@gmail.com>
2021-05-01 15:56:10 +0900

    edit.. don't know

--- cpp5622.cpp
+++ cpp5622.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-	
+	char num; int dial;
+
+	while ( (num = getchar()) - 0x0A ) {
+		for(char c : "zzADGJMPTW" )
+			dial += num >= c;
+		dial++;
+
+	}
+	cout << dial << endl;
 	return 0;
 }
