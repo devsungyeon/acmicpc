f9921fb8e4c9e0fd9997ebcc9795065b27499f9d
devsungyeon <sungyeonlee1103@gmail.com>
2021-05-03 10:59:29 +0000

    commit 1712

--- c1712.c
+++ c1712.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	int i = 0 ;
+	if(b>=c) {
+		printf("-1");
+		return 0;
+	}
 	
+	printf("%d", (a/(c-b)+1));
 	return 0;
 }
