26da9d92b13c6457399bdc5ab80e7900401171c2
devsyl <sungyeonlee1103@gmail.com>
2021-05-11 20:23:18 +0900

    2231 done

--- cpp2231.cpp
+++ cpp2231.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+int sang(int i, int n) {
+	int temp = i;
+	while(i) {
+		temp += i%10;
+		i /= 10;
+	}
+	if(temp == n) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
 int main() {
 	int n;
 	cin >> n;
-
+	int ans=0;
+	for(int i = 1; i <=n ; i++ ) {
+		if(sang(i, n)) {
+			ans = i;
+			break;
+		}
+	}
+	cout << ans << endl;
 	return 0;
 }
