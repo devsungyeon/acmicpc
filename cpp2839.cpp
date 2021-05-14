3595661b8f880ff09b946f326fc26b078a574dbd
devsyl <sungyeonlee1103@gmail.com>
2021-05-14 20:48:27 +0900

    2839 done

--- cpp2839.cpp
+++ cpp2839.cpp
@@ -5,14 +5,21 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	int f, t;
-	f = n/5;
-	n -= f*5;
-	t = n/3;
-	n -= t*3;
-	if(n==0)
-		cout << f+t << endl;
-	else
+	int ans = 5000;
+	for(int i = 0 ; i <= 1000 ; i++ ) {
+		int temp = n;
+		temp -= i*5;
+		if(temp < 0)
+			break;
+		int divi = temp / 3;
+		int mod = temp % 3;
+		if(mod == 0)
+			if(ans > divi+i )
+				ans = divi + i;
+	}
+	if(ans==5000)
 		cout << -1 << endl;
+	else
+		cout << ans << endl;
 	return 0;
 }
