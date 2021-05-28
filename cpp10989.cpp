d9a79a594c4905cf5998cf042509ec6d262b9a13
devsungyeon <sungyeonlee1103@gmail.com>
2021-05-28 13:00:00 +0900

    today done

--- cpp10989.cpp
+++ cpp10989.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 
 int main() {
-	
+	int n;
+	cin >> n;
+	int arr[10000001] ;
+	for(int i=1;i<10000001;i++)
+	{
+		arr[i] = 0;
+	}
+	for(int i = 0 ; i<n; i++)
+	{
+		int temp;
+		cin >> temp;
+		arr[temp]++;
+	}
+	cout << "test" << endl;
+	/*
+	for(int i=1;i<10000001;i++)
+	{
+		if(arr[i] != 0)
+		{
+			for(int j=0;j<arr[i];j++)
+				cout << i << endl;
+		}
+	}
+*/
 	return 0;
 }
